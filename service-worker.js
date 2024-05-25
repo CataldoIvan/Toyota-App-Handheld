75d7ef65b1e1b6231e418fef43e920f4ab2f3885
cataldoMarcos <cmiarg@gmail.com>
2024-05-25 11:54:40 -0300

    funcionalidad pwa

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'my-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/check-tags.html',
+  '/log.html',
+  '/toyotaicon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
